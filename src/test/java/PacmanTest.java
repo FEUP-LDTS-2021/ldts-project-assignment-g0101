5a6ae5d4c848275ff00058e35cee15c21bc4f1f3
Hercules Konsoulas <dyka3773@gmail.com>
2021-12-27 18:54:24 +0000

    Added some TODOs to help us

--- src/test/java/PacmanTest.java
+++ src/test/java/PacmanTest.java
@@ -2,4 +2,5 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PacmanTest {
+    //TODO: Implement tests for the functionalities described in the text files
 }
--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -2,4 +2,5 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
+    //TODO: Implement tests for the functionalities described in the text files
 }
