56a476c7dc4f2716cfaa23d92c1924d16adaed98
Aires <joaoaires98@gmail.com>
2022-01-14 13:40:23 +0000

    update

--- src/test/java/PacmanTest.java
+++ src/test/java/PacmanTest.java
@@ -1,6 +1,26 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PacmanTest {
     //TODO: Implement tests for the functionalities described in the text files
+
+    Element pac;
+    Game game;
+
+    @BeforeEach
+    public void createPac(){
+        game = new Game();
+        pac = new Pacman(10,10);
+    }
+
+    @Test
+    public void exists(){
+        Assertions.assertNotNull(pac);
+    }
+
+    public void moveLeft(){
+
+    }
 }
