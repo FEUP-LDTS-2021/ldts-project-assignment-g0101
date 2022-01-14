93249e2c452ecf84cef70b728108d0ce08e6ba43
Diogo Nunes <diogoffn@gmail.com>
2022-01-14 07:18:22 -0800

    Implemented draw in Ghost.java

--- src/main/java/Ghost.java
+++ src/main/java/Ghost.java
@@ -1,3 +1,7 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Ghost extends GameCharacter {
@@ -10,6 +14,9 @@ public class Ghost extends GameCharacter {
 
     @Override
     public void draw(TextGraphics graphics) {
-        //TODO: Draw Ghost
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
+        graphics.setCharacter(position.getX(), position.getY(), TextCharacter.fromCharacter('F')[0]);
     }
 }
