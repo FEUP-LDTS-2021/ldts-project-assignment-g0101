1244938a2b7b8d3986a040254c88eb5ce573a37e
Aires <joaoaires98@gmail.com>
2022-01-17 14:46:13 +0000

    ghost movement tests

--- src/test/java/GhostTest.java
+++ src/test/java/GhostTest.java
@@ -2,16 +2,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class GhostTest {
     //TODO: Implement tests for the functionalities described in the text files
 
     Ghost ghost;
-    Game game;
+    Maze maze;
 
     @BeforeEach
     public void createGhost(){
-        game = new Game();
-        ghost = new Ghost(10,10);
+        MapReader mapReader = new MapReader(new File("ghostmovement.txt"));
+        maze = new Maze(mapReader.readMap());
+        ghost = new Ghost(2,2);
     }
 
     @Test
@@ -20,5 +23,39 @@ public class GhostTest {
         Assertions.assertNotNull(ghost);
     }
 
+    @Test
+    public void moveLeft(){
+        Position start = ghost.getPosition();
+        ghost.moveLeft();
+        Position end = ghost.getPosition();
+        Assertions.assertEquals(start.getY(),end.getY());
+        Assertions.assertEquals(start.getX()-1,end.getX());
+    }
 
+    @Test
+    public void moveRight(){
+        Position start = ghost.getPosition();
+        ghost.moveRight();
+        Position end = ghost.getPosition();
+        Assertions.assertEquals(start.getY(),end.getY());
+        Assertions.assertEquals(start.getX()+1,end.getX());
+    }
+
+    @Test
+    public void moveUp(){
+        Position start = ghost.getPosition();
+        ghost.moveUp();
+        Position end = ghost.getPosition();
+        Assertions.assertEquals(start.getY()-1,end.getY());
+        Assertions.assertEquals(start.getX(),end.getX());
+    }
+
+    @Test
+    public void moveDown(){
+        Position start = ghost.getPosition();
+        ghost.moveDown();
+        Position end = ghost.getPosition();
+        Assertions.assertEquals(start.getY()+1,end.getY());
+        Assertions.assertEquals(start.getX(),end.getX());
+    }
 }
