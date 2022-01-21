934a56f1f15b9618a48b5b4c499c13c53e54dbfe
IAmSHOCK <joaoaires98@gmail.com>
2022-01-21 17:52:04 +0000

    font error isn't monospaced. can't fix it

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,31 +1,60 @@
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Game {
     private Screen screen;
     private Maze maze;
 
-    public Game() {
+    public Game() throws IOException, FontFormatException, URISyntaxException {
         try {
-            TerminalSize terminalSize = new TerminalSize(28, 30);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
-                    .setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
+
+            URL resource = getClass().getClassLoader().getResource("font.ttf");
+            File fontFile = new File(resource.toURI());
+            Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+
+            DefaultTerminalFactory factory = new DefaultTerminalFactory();
+
+            Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+            AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+            factory.setTerminalEmulatorFontConfiguration(fontConfig);
+            factory.setForceAWTOverSwing(true);
+            factory.setInitialTerminalSize(new TerminalSize(28, 30));
+
+            Terminal terminal = factory.createTerminal();
+            ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    e.getWindow().dispose();
+                }
+            });
 
             this.screen = new TerminalScreen(terminal);
-
             this.screen.setCursorPosition(null);   // we don't need a cursor
             this.screen.startScreen();             // screens must be started
-            this.screen.doResizeIfNecessary();     // resize screen if necessary
+            this. screen.doResizeIfNecessary();     // resize screen if necessary
+
+            //this. screen.setCharacter(10, 10, TextCharacter.fromCharacter('C')[0]);
+            //this.screen.refresh();
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
