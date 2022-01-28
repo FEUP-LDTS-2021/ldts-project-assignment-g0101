ab47fa31e105d2b7892eca3fe75207500f3d87e9
IAmSHOCK <joaoaires98@gmail.com>
2022-01-28 12:37:31 +0000

    update

--- src/test/java/PointTest.java
+++
@@ -1,6 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class PointTest {
-    //TODO: Implement tests for the functionalities described in the text files
-}
